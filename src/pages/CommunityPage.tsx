c96e49242136ac3527cb515afeb75f0b59c0c163
Ali Yamani <aliyamanii@outlook.com>
2023-10-06 08:11:08 +0000

    Community page

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -1,72 +1,73 @@
-import React, { useState } from "react";
-import sampleData from "../assets/sampleData";
-import pfp from "../assets/photos/samplePosts/dragon.jpg";
-import ellipsis from "../assets/photos/ellipsis.svg";
+import React from "react";
+import { useExploreDataQuery } from "../api/user";
+import { useNavigate, useParams } from "react-router-dom";
+import UserBadge from "../components/UserBadge";
+import SpinnerIcon from "../assets/photos/spinner.svg";
+
+interface ExploreItem {
+  id: string;
+  followers: number;
+  username: string;
+  posts: {
+    id: string;
+    userId: string;
+    image: {
+      id: string;
+      path: string;
+    };
+  }[];
+}
 
 function CommunityPage() {
-  const { data } = sampleData;
+  const { data, isLoading, isError } = useExploreDataQuery();
+  const navigate = useNavigate();
 
-  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  if (isLoading) {
+    return (
+      <div className="h-full w-full flex justify-center items-center">
+        <img src={SpinnerIcon} alt="" className="animate-spin" />
+      </div>
+    );
+  }
 
-  const toggleDropDown = () => {
-    setIsDropDownOpen((prev) => !prev);
-  };
+  if (isError) {
+    // navigate("/error", { replace: true });
+    return null;
+  }
 
   return (
     <div className="mr-16 font-primary">
-      <h1 className="w-full mr-16 flex flex-col text-right text-[22px] font-bold justify-start font-primary   ">
+      <h1 className="w-full mr-16 flex flex-col text-right text-[22px] font-bold justify-start">
         کالج گرامی ها
       </h1>
       <div className="w-full h-[650px] mr-16 overflow-y-scroll no-scrollbar  justify-center items-center">
-        {data.items.map((item) => (
-          <div key={item.user.id}>
-            {item.posts.map((post) => (
-              <div
-                key={post.id}
-                className="flex flex-col justify-end items-end"
-              >
-                <div className="flex items-end justify-end">
+        {data.items.map((item: ExploreItem) => (
+          <div key={item.id}>
+            <div className="flex flex-col justify-end items-end">
+              <div className="flex items-end justify-end">
+                {item.posts.map((post) => (
                   <img
-                    src={post.images[0].path}
-                    alt="Post Image 1"
+                    key={post.id}
+                    src={post.image.path}
+                    alt="Post Image"
                     className="w-[230px] h-[230px] object-cover m-2 rounded-[24px] hover:scale-105 transition-all duration-300"
                   />
-                  <img
-                    src={post.images[1].path}
-                    alt="Post Image 2"
-                    className="w-[230px] h-[230px] object-cover m-2 rounded-[24px] hover:scale-105 transition-all duration-300"
-                  />
-                  <img
-                    src={post.images[2].path}
-                    alt="Post Image 3"
-                    className="w-[230px] h-[230px] object-cover m-2 rounded-[24px] hover:scale-105 transition-all duration-300"
-                  />
-                </div>
-
-                <div className="flex items-center gap-4">
-                  <img
-                    src={ellipsis}
-                    alt={`options`}
-                    className="w-[18px] h-[18px] hover:scale-125 transition-all duration-300 cursor-pointer"
-                    onClick={toggleDropDown}
-                  />
-                  <div>
-                    <div className="text-[16px] font-semibold text-center leading-[26px] text-amber">
-                      {"something"}
-                    </div>
-                    <div className="flex text-navy">
-                      <div> دنبال کننده </div>
-                      {"3"}
-                    </div>
-                  </div>
-                  <img
-                    src={pfp}
-                    alt={`someone's Profile`}
-                    className="w-[80px] h-[80px] p-1 rounded-full object-cover "
-                  />
-                </div>
+                ))}
               </div>
-            ))}
+
+              <div className="flex items-center gap-4">
+                <div>
+                  <div className="text-[16px] font-semibold text-center leading-[26px] text-amber">
+                    {item.username}
+                  </div>
+                  <div className="flex text-navy">
+                    <div> دنبال کننده </div>
+                    {item.followers}
+                  </div>
+                </div>
+                <UserBadge userId={item.id} />
+              </div>
+            </div>
           </div>
         ))}
       </div>
